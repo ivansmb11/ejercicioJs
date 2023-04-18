41712d767119e30e98aec8de218ea6310c7a3d6d
ivansmb11 <ismb9@hotmail.com>
2023-04-17 20:00:53 -0600

    juego 2

--- js/juego.js
+++ js/juego.js
@@ -3,7 +3,118 @@ const inicializa = () => {
 	console.log("Cargado");
 }
 
-// let vida = 100;
+let vida = 100;
+let estadoJugador = 'Sano';
+let estados = ['Sano', 'Quemado', 'Envenenado', 'Muerto'];
+let items = ['Pocion', 'Cola de Fénix', 'Pocion Quemadura', 'Pocion Envenenamiento'];
+
+const jugarTurno = () => {
+	if (vida <= 0) {
+		muerte();
+	} else {
+		// const accion = Math.floor(Math.random() * 4) + 1;
+		const accion = 1;
+		console.log(accion);
+		switch (accion) {
+			case 1:
+				accionAtaque();
+				break;
+			case 2:
+				accionQuemar();
+				break;
+			case 3:
+				accionEnvenenar();
+				break;
+			case 4:
+				console.log("El ataque enemigo ha fallado, que suerte.");
+				break;
+		}
+	}
+}
+
+const accionAtaque = () => {
+	vida -= 10;
+	console.log("Has sido atacado por 10 puntos de vida!\n");
+	muestraEstadoJugador();
+}
+
+const accionQuemar = () => {
+	estadoJugador = estados[1];
+	muestraEstadoJugador();
+}
+
+const accionEnvenenar = () => {
+	estadoJugador = estados[2];
+	muestraEstadoJugador();
+}
+
+const muerte = () => {
+	estadoJugador = estados[3];
+	console.log("Has muerto");
+}
+
+const muestraEstadoJugador = () => {
+	switch (estadoJugador) {
+		case estados[0]:
+			console.log("ESTADO: Estas sano.\nTines " + vida + " puntos de vida.");
+			break;
+		case estados[1]:
+			console.log("ESTADO: Estas quemado, usa un item!\nTines " + vida + " puntos de vida.");
+			break;
+		case estados[2]:
+			console.log("ESTADO: Estas envenenado, usa un item!\nTines " + vida + " puntos de vida.");
+			break;
+		case estados[3]:
+			muerte();
+			break;
+	}
+}
+
+const colaFenix = () => {
+	console.log(vida);
+	if (vida == 0) {
+		vida = 30;
+		estadoJugador = estados[0];
+		console.log("Has usado una cola de fenix, haz vuelto a la vida!\n");
+		muestraEstadoJugador();
+	} else {
+		console.log("No puedes usar una cola de fenix si estás vivo.\n");
+	}
+}
+
+const usarItem = (tipo) => {
+	switch (tipo) {
+		case items[0]:
+			vida += 50;
+			console.log("Has usado una poción!\n");
+			muestraEstadoJugador();
+			break;
+		case items[1]:
+			colaFenix();
+			muestraEstadoJugador();
+			break;
+		case items[2]:
+			if (estadoJugador == estados[1]) {
+				estadoJugador = estados[0];
+				console.log("Has usado una pocion de quemadura.\n");
+				muestraEstadoJugador();
+			} else {
+				console.log("No puedes usar una pocion de quemadura si no estas quemado.\n");
+			}
+			break;
+		case items[3]:
+			if (estadoJugador == estados[2]) {
+				estadoJugador = estados[0];
+				console.log("Has usado una pocion de envenenamiento.\n");
+				muestraEstadoJugador();
+			} else {
+				console.log("No puedes usar una pocion de envenenamiento si no estas envenenado.\n");
+			}
+			break;
+	}
+}
+
+
 
 // const ataqueEnemigo = ( potencia, nombreAtaque ) => {
 // 	vida -= potencia;
