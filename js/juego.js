fdcbecd38fc4c1cbe07bfa16f299a4ce1b9e26dc
ivansmb11 <ismb9@hotmail.com>
2023-04-19 20:00:33 -0600

    ES5 version

--- js/juego.js
+++ js/juego.js
@@ -3,48 +3,27 @@ const inicializa = () => {
 	console.log("Cargado");
 }
 
-const articulos = ['Pocion', 'Magia', 'Espada', 'Arco', 'Flecha'];
+let configTeclado = { prevent_repeat : true };
+let eventoTeclado = new window.keypress.Listener(this,configTeclado);
 
-const comprar = ( articulo ) => {
-	switch ( articulo ) {
-		case articulos[0]:
-			console.log("Has comprado una pocion");
-			break;
-		case articulos[1]:
-			console.log("Has comprado una magia");
-			break;
-		case articulos[2]:
-			console.log("Has comprado una espada");
-			break;
-		case articulos[3]:
-			console.log("Has comprado un arco");
-			break;
-		case articulos[4]:
-			console.log("Has comprado una flecha");
-			break;
-		default:
-			console.log("No has comprado nada");
-	}
+let miCanvas = document.getElementById('canvas');
+miCanvas.addEventListener('mousedown', clicRaton, true);
+miCanvas.addEventListener('mousemove', posicionRaton, true);
+
+const posicionRaton = (e) => {
+	console.log("X: " + e.pageX + " \nY: " + e.pageY);
 }
 
-const crearEnemigos = ( noEnemigos ) => {
-	let enemigos = 0;
-	for ( let i = 0; i < noEnemigos; i++ ) {
-		enemigos++;
-	}
-	console.log( 'Haz creado ' + enemigos + ' enemigos.' );
+const clicRaton = (e) => {
+	console.log("Has pulsado el ratón.");
 }
 
-const ciclo = () => {
-	const numeroCPU = Math.floor(Math.random() * 10) + 1;
-	console.log(numeroCPU);
-	do {
-		const valor1 = parseInt(prompt("Introduce un número"));
+eventoTeclado.simple_combo('a', () => {
+	console.log("Has pulsado la tecla A");
+});
 
-		if ( numeroCPU == valor1 ) {
-			console.log("Has ganado");
-		} else {
-			console.log("Has perdido");
-		}
-	} while ( numeroCPU != valor1 );
-}
+eventoTeclado.simple_combo('b', () => {
+	console.log("Has pulsado la tecla B");
+});
+
+
