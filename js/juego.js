9d6a5bed152587695c27c7c84d0a06f9bf8c58ac
ivansmb11 <ismb9@hotmail.com>
2023-04-24 19:49:50 -0600

    arrays

--- js/juego.js
+++ js/juego.js
@@ -1,35 +1,13 @@
+var mochila = ['A', 'B', 'C', 'D'];
 
-const inicializa = () => {
-	console.log("Cargado");
-}
+    function vender() {
+        mochila.splice(3, 1);
+    }
 
-let vida = 100;
+    function compra(item) {
+        mochila.push(item);
+    }
 
-const ataqueEnemigo = ( potencia, nombreAtaque ) => {
-	vida -= potencia;
-	muestraVida("Ataque", potencia, nombreAtaque);
-}
-
-const bebePocion = ( pocion ) => {
-	vida += pocion;
-	muestraVida("Pocion", pocion, "");
-}
-
-const muestraVida = ( tipo, dañoVida, nombreAtaque ) => {
-	if (tipo == "Pocion") {
-		console.log("Has bebido una pocion de " + dañoVida + " puntos de vida");
-	} else {
-		console.log("Has recibido el ataque de " + nombreAtaque + " por " + dañoVida + " puntos de vida");
-	}
-	console.log ("Vida: " + vida);
-}
-
-const accionPrompt = () => {
-	let valor1 = prompt("Introduce un número");
-	let valor2 = prompt("Introduce otro número");
-
-	let resultado = parseInt(valor1) + parseInt(valor2);
-
-	console.log("El resultado es: " + resultado);
-}
-
+    function muestraInventario() {
+        console.log(mochila);
+    }
